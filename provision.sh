7ac1a8d1794656bda376b6cc03974f23d242deab
andy c <t2ahc@sad-panda.local>
2013-09-05 19:55:57 -0400

    add C++ R Clojure to provisioning file

--- provision.sh
+++ provision.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 sudo apt-get update
+sudo apt-get -y install g++
+sudo apt-get -y install gdb
 sudo apt-get -y install emacs
 sudo apt-get -y install make
 sudo apt-get -y install git
+
 sudo apt-get -y install python-numpy
 sudo apt-get -y install python-scipy
 sudo apt-get -y install python-matplotlib
@@ -12,5 +15,8 @@ sudo apt-get -y install ipython-notebook
 sudo apt-get -y install python-pandas
 sudo apt-get -y install python-sympy
 sudo apt-get -y install python-nose
+
+sudo apt-get -y install leiningen
+sudo apt-get -y install r-base
 
 exit 0
