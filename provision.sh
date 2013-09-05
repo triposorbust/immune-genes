49aa80e52f0d0da37d2ced0c0ff47f3c4c719a68
andy c <t2ahc@sad-panda.local>
2013-09-05 19:49:13 -0400

    add vagrant file and python provisioning

---
+++ provision.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get -y install emacs
+sudo apt-get -y install make
+sudo apt-get -y install git
+sudo apt-get -y install python-numpy
+sudo apt-get -y install python-scipy
+sudo apt-get -y install python-matplotlib
+sudo apt-get -y install ipython
+sudo apt-get -y install ipython-notebook
+sudo apt-get -y install python-pandas
+sudo apt-get -y install python-sympy
+sudo apt-get -y install python-nose
+
+exit 0
