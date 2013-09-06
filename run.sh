31140db14baaa1a7791a5659fe8391d86b5aaf82
andy c <t2ahc@sad-panda.local>
2013-09-06 15:12:58 -0400

    ignore final output files and pipe stderr to logfile

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python extract-data.py
-python normalize-data.py
-python find-immune-genes.py
+python extract-data.py 2>> errors.log
+python normalize-data.py 2>> errors.log
+python find-immune-genes.py 2>> errors.log
 
 echo `cat immune-genes.tsv | wc -l` "immune genes found"
 
